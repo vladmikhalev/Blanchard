e5bebcff47f10a017938913ffa4bc9323628f2c2
Vladislav Mikhalev <v.a.mikhalev@mail.ru>
2022-09-20 21:13:18 +0300

    work_day_2

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// select in header
+
+// const element = document.querySelector('.search__select');
+// const choices = new Choices(element, {
+//   searchEnabled: false,
+//   allowHTML: true,
+//   position: 'bottom',
+//   shouldSort: false,
+
+// });
+
+// scrollbar 
+document.querySelectorAll(".dropdown__simplbar").forEach(dropdown => {
+  new SimpleBar(dropdown, {
+  /* чтобы изначально ползунок был виден */
+  autoHide: false,
+  /* с помощью этого значения вы можете управлять высотой ползунка*/
+  scrollbarMaxSize: 28,
+});
+})
+
+const btns = document.querySelectorAll(".search__btn");
+const dropdowns = document.querySelectorAll(".dropdown");
+const activeClassdropdowns = "dropdown__active";
+const activeClassbtns = "btn__active";
+
+btns.forEach(item => {
+  item.addEventListener("click", function () {
+    let DropThis = this.parentElement.querySelector(".dropdown");
+    dropdowns.forEach(el => {
+      if (el != DropThis) {
+        el.classList.remove(activeClassdropdowns)
+      }
+    });
+    btns.forEach(el => {
+      if (el != this) {
+        el.classList.remove(activeClassbtns)
+      }
+    });
+    DropThis.classList.toggle(activeClassdropdowns);
+    this.classList.toggle(activeClassbtns);
+  })
+})
